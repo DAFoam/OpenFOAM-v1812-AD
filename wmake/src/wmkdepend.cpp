47ea72171968f259735ac88f5c65822ee1f4b2ea
Ping He <phe@iastate.edu>
2020-11-10 15:45:37 -0600

    Fixed an issue for wmkdepend.

--- wmake/src/wmkdepend.cpp
+++ wmake/src/wmkdepend.cpp
@@ -283,13 +283,14 @@ namespace Files
         visited.insert(fileName);
 
         // Report failures
+        // CoDiPack4OpenFOAM Always depress the "could not open" warning.
+        /*
         if (!infile && !optQuiet)
         {
-            // CoDiPack4OpenFOAM Depress this warning
-            //std::cerr
-            //    << EXENAME ": could not open '"
-            //    << fileName << "' for source file '"
-            //    << sourceFile << "'";
+            std::cerr
+                << EXENAME ": could not open '"
+                << fileName << "' for source file '"
+                << sourceFile << "'";
 
             if (dirs.size())
             {
@@ -298,6 +299,7 @@ namespace Files
 
             std::cerr << '\n' << std::flush;
         }
+        */
 
         return infile;
     }
