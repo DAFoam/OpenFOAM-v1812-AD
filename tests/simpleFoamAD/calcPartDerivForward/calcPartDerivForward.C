95b74fb0d95093b790d4e0edc561ca90f7379368
Ping He <phe@iastate.edu>
2020-11-21 17:27:35 -0600

    Update.

--- tests/simpleFoamAD/calcPartDerivForward/calcPartDerivForward.C
+++ tests/simpleFoamAD/calcPartDerivForward/calcPartDerivForward.C
@@ -108,6 +108,75 @@ int main(int argc, char *argv[])
         // Test Proc0 CellI=61, 84
         // Proc1 CellI=59
 
+        // cellI = 61
+        label cellI = 61;
+        {
+            if (myProc == 0)
+            {
+                U[cellI][0].setGradient(1.0);
+            }
+    
+            fvVectorMatrix UEqn(
+                fvm::div(phi, U)
+                + turbulence->divDevReff(U));
+            UEqn.relax();
+    
+            volVectorField URes = (UEqn & U) + fvc::grad(p);
+    
+            word partDerivResName = "verify_AD_CellI"+name(cellI)+"_n"+name(myProc);
+            OFstream fOut(partDerivResName);
+            forAll(URes, idxJ)
+            {
+                for(label j=0;j<3;j++)
+                {
+                    scalar deriv = URes[idxJ][j].getGradient();
+                    if (fabs(deriv) > 1e-16) fOut<<deriv<<endl;
+                    else fOut<<"0"<<endl;
+                }
+            }
+        }
+
+        {
+            fvVectorMatrix UEqn(
+                fvm::div(phi, U)
+                + turbulence->divDevReff(U));
+            UEqn.relax();
+    
+            volVectorField URes = (UEqn & U) + fvc::grad(p);
+
+            if (myProc == 0)
+            {
+                U[cellI][0]+=eps;
+            }
+            U.correctBoundaryConditions();
+
+            fvVectorMatrix UEqnP(
+                fvm::div(phi, U)
+                + turbulence->divDevReff(U));
+
+            UEqnP.relax();
+
+            volVectorField UResP = (UEqnP & U) + fvc::grad(p);
+
+            if (myProc == 0)
+            {
+                U[cellI][0] -= eps;
+            }
+            U.correctBoundaryConditions();
+            
+            word partDerivResName = "verify_FD_CellI"+name(cellI)+"_n"+name(myProc);
+            OFstream fOut(partDerivResName);
+            forAll(URes, idxJ)
+            {
+                for(label j=0;j<3;j++)
+                {
+                    scalar deriv = (UResP[idxJ][j] - URes[idxJ][j] ) /eps;
+                    if (fabs(deriv) > 1e-16) fOut<<deriv<<endl;
+                    else fOut<<"0"<<endl;
+                }
+            }    
+        }
+
     }
 
     return 0;
