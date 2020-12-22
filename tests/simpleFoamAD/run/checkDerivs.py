99efb9a91d197854816764cee67477870033eca2
Ping He <phe@iastate.edu>
2020-12-21 21:29:20 -0600

    Update.

--- tests/simpleFoamAD/run/checkDerivs.py
+++ tests/simpleFoamAD/run/checkDerivs.py
@@ -1,42 +1,64 @@
+"""
+Dot product tests for forward and reverse mode AD
+We also compare matrix-vector products between forward AD and FD
+"""
+
 import numpy as np
 from numpy import linalg as LA
+import sys
 
-stateFADVal = open("dRdWPsi_0_AD_Values.txt", "r")
-stateFADSeed = open("dRdWPsi_0_AD_Seeds.txt", "r")
-stateFDVal = open("dRdWPsi_0_FD_Values.txt", "r")
-stateRADVal = open("dRdWTPsi_0_AD_Values.txt", "r")
-stateRADSeed = open("dRdWTPsi_0_AD_Seeds.txt", "r")
+nProcs = sys.argv[1]
 
-stateFADValLines = stateFADVal.readlines()
-stateFADSeedLines = stateFADSeed.readlines()
-stateFDValLines = stateFDVal.readlines()
-stateRADValLines = stateRADVal.readlines()
-stateRADSeedLines = stateRADSeed.readlines()
+dot1 = 0.0
+dot2 = 0.0
 
-stateFADValList = []
-stateFADSeedList = []
-stateFDValList = []
-stateRADValList = []
-stateRADSeedList = []
-for idx in range(len(stateFADValLines)):
-    stateFADValList.append(float(stateFADValLines[idx]))
-    stateFADSeedList.append(float(stateFADSeedLines[idx]))
-    stateFDValList.append(float(stateFDValLines[idx]))
-    stateRADValList.append(float(stateRADValLines[idx]))
-    stateRADSeedList.append(float(stateRADSeedLines[idx]))
+for n in range(nProcs):
 
-stateFADValList = np.asarray(stateFADValList)
-stateFADSeedList = np.asarray(stateFADSeedList)
-stateFDValList = np.asarray(stateFDValList)
-stateRADValList = np.asarray(stateRADValList)
-stateRADSeedList = np.asarray(stateRADSeedList)
+    print("Processing %d" % n)
 
-diffADFD = stateFADValList - stateFDValList
+    stateFADVal = open("dRdWPsi_%d_AD_Values.txt" % n, "r")
+    stateFADSeed = open("dRdWPsi_%d_AD_Seeds.txt" % n, "r")
+    stateFDVal = open("dRdWPsi_%d_FD_Values.txt" % n, "r")
+    stateRADVal = open("dRdWTPsi_%d_AD_Values.txt" % n, "r")
+    stateRADSeed = open("dRdWTPsi_%d_AD_Seeds.txt" % n, "r")
 
-dot1 = np.dot(stateFDValList, stateRADSeedList)
-dot2 = np.dot(stateRADValList, stateFADSeedList)
+    stateFADValLines = stateFADVal.readlines()
+    stateFADSeedLines = stateFADSeed.readlines()
+    stateFDValLines = stateFDVal.readlines()
+    stateRADValLines = stateRADVal.readlines()
+    stateRADSeedLines = stateRADSeed.readlines()
 
-print("maxDiff: ", diffADFD.max())
-print("normDiff: ", LA.norm(diffADFD))
-print("MVF", dot1)
-print("MVR", dot2)
+    stateFADVal.close()
+    stateFADSeed.close()
+    stateFDVal.close()
+    stateRADVal.close()
+    stateRADSeed.close()
+
+    stateFADValList = []
+    stateFADSeedList = []
+    stateFDValList = []
+    stateRADValList = []
+    stateRADSeedList = []
+    for idx in range(len(stateFADValLines)):
+        stateFADValList.append(float(stateFADValLines[idx]))
+        stateFADSeedList.append(float(stateFADSeedLines[idx]))
+        stateFDValList.append(float(stateFDValLines[idx]))
+        stateRADValList.append(float(stateRADValLines[idx]))
+        stateRADSeedList.append(float(stateRADSeedLines[idx]))
+
+    stateFADValList = np.asarray(stateFADValList)
+    stateFADSeedList = np.asarray(stateFADSeedList)
+    stateFDValList = np.asarray(stateFDValList)
+    stateRADValList = np.asarray(stateRADValList)
+    stateRADSeedList = np.asarray(stateRADSeedList)
+
+    diff = stateFADValList - stateFDValList
+
+    dot1 += np.dot(stateFDValList, stateRADSeedList)
+    dot2 += np.dot(stateRADValList, stateFADSeedList)
+
+    print("Max diff in dRdW*Psi: ", diff.max())
+    print("Diff norm in dRdW*Psi: ", LA.norm(diff))
+
+print("Dot-product 1: ", dot1)
+print("Dot-product 2: ", dot2)
